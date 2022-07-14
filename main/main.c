36e02acf35ecd6d623ed40714bcbd23220bcf8b2
Ahmedkhalifa1999 <ahmedkhalifa1999@gmail.com>
2022-07-14 20:25:14 +0200

    Discovers Surrounding BT devices

--- main/main.c
+++ main/main.c
@@ -12,6 +12,33 @@
 #include "esp_log.h"
 
 
+void bt_callback(esp_bt_gap_cb_event_t event, esp_bt_gap_cb_param_t *param)
+{
+    ESP_LOGI("BT", "Event: %d", event);
+    switch (event)
+    {
+        case ESP_BT_GAP_DISC_RES_EVT:
+            esp_bt_gap_read_remote_name(param->disc_res.bda);
+            break;
+            
+        case ESP_BT_GAP_DISC_STATE_CHANGED_EVT:
+             if (param->disc_st_chg.state == ESP_BT_GAP_DISCOVERY_STOPPED)
+            {
+                ESP_LOGI("BT", "Discovery Restarted");
+                esp_bt_gap_start_discovery(ESP_BT_INQ_MODE_GENERAL_INQUIRY, 5, 0);
+            }
+            break;
+
+        case ESP_BT_GAP_READ_REMOTE_NAME_EVT:
+            ESP_LOGI("BT", "Discovered %s", param->read_rmt_name.rmt_name);
+            break;
+
+        default:
+            break;
+    }
+}
+
+
 void app_main(void)
 {
      esp_err_t ret = nvs_flash_init();
@@ -39,5 +66,8 @@ void app_main(void)
         return;
     }
     esp_bt_dev_set_device_name("My ESP32");
-    esp_bt_gap_set_scan_mode(ESP_BT_NON_CONNECTABLE, ESP_BT_GENERAL_DISCOVERABLE);
+    esp_bt_gap_set_scan_mode(ESP_BT_CONNECTABLE, ESP_BT_LIMITED_DISCOVERABLE);
+
+    esp_bt_gap_register_callback(bt_callback);
+    esp_bt_gap_start_discovery(ESP_BT_INQ_MODE_LIMITED_INQUIRY, 0x10, 0);
 }
